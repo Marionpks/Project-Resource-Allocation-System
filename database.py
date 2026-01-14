b5eb89c8d18d3298977b6044571709201cb4aee1
Yampss <chriss.16hacker@gmail.com>
2026-01-14 20:21:16 +0530

    database and models

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import sessionmaker , declarative_base
+from sqlalchemy import create_engine
+
+DATABASE_URL="sqlite:///./capstone.db"
+
+engine=create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
+
+sessionlocal=sessionmaker(autocommit= False, autoflush= False ,bind=engine)
+
+base=declarative_base()
---
+++ models.py
@@ -0,0 +1,28 @@
+from database import base ,engine
+
+from sqlalchemy import Column , String , Integer ,ForeignKey
+
+class EmployeeDB(base):
+    __tablename__="employeedb"
+    employee_id=Column(Integer,primary_key=True,index=True,autoincrement=True)
+    employee_name=Column(String)
+    skilled_language=Column(String)
+    available_hrs=Column(Integer)
+
+class ProjectDB(base):
+    __tablename__="projectdb"
+    project_id=Column(Integer,primary_key=True,index=True,autoincrement=True)
+    project_name=Column(String)
+    project_duration=Column(Integer)
+    project_skill_required=Column(String)
+
+class AllocationDB(base):
+    __tablename__="allocationdb"
+    allocation_id=Column(Integer, primary_key=True , index=True , autoincrement=True)
+    project_id=Column(Integer,ForeignKey(ProjectDB.project_id))
+    employee_id=Column(Integer,ForeignKey(EmployeeDB.employee_id))
+    allocation_hours=Column(Integer,default=0)
+
+
+
+
